5e2977914dd349d2cff15b7ee1d961b4d8b9d164
LiHaifeng <LiHaifeng@163.com>
2018-05-19 18:23:46 +0800

    增加API validList到Abnormal后台服务中

--- src/main/java/com/eservice/api/web/AbnormalController.java
+++ src/main/java/com/eservice/api/web/AbnormalController.java
@@ -1,4 +1,5 @@
 package com.eservice.api.web;
+
 import com.eservice.api.core.Result;
 import com.eservice.api.core.ResultGenerator;
 import com.eservice.api.model.abnormal.Abnormal;
@@ -14,14 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 import tk.mybatis.mapper.entity.Condition;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 /**
-* Class Description: xxx
-* @author Wilson Hu
-* @date 2018/03/19.
-*/
+ * Class Description: xxx
+ *
+ * @author Wilson Hu
+ * @date 2018/03/19.
+ */
 @RestController
 @RequestMapping("/abnormal")
 public class AbnormalController {
@@ -32,7 +35,7 @@ public class AbnormalController {
 
     @PostMapping("/add")
     public Result add(Abnormal abnormal) {
-        if(abnormal != null) {
+        if (abnormal != null) {
             abnormal.setCreateTime(new Date());
         }
         abnormalService.save(abnormal);
@@ -41,14 +44,14 @@ public class AbnormalController {
 
     @PostMapping("/delete")
     public Result delete(@RequestParam Integer id) {
-        if(id == null || id <= 0) {
+        if (id == null || id <= 0) {
             return ResultGenerator.genFailResult("异常ID有误！");
-        }else {
+        } else {
             //检查异常记录中是否使用了该异常，如果已经使用，不能删除
             Condition condition = new Condition(AbnormalRecord.class);
             condition.createCriteria().andCondition("abnormal_type = ", id);
             List<AbnormalRecord> list = abnormalRecordService.findByCondition(condition);
-            if( list == null || list.size() > 0) {
+            if (list == null || list.size() > 0) {
                 return ResultGenerator.genFailResult("该异常已经被使用，删除会导致之前的y异常记录有误；如不想使用，可把该项设置为无效!");
             }
         }
@@ -58,7 +61,7 @@ public class AbnormalController {
 
     @PostMapping("/update")
     public Result update(Abnormal abnormal) {
-        if(abnormal != null) {
+        if (abnormal != null) {
             abnormal.setUpdateTime(new Date());
             abnormalService.update(abnormal);
             return ResultGenerator.genSuccessResult();
@@ -78,6 +81,21 @@ public class AbnormalController {
         PageHelper.startPage(page, size);
         List<Abnormal> list = abnormalService.findAll();
         PageInfo pageInfo = new PageInfo(list);
+        return ResultGenerator.genSuccessResult(pageInfo);
+    }
+
+    @PostMapping("/validList")
+    public Result validList(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
+        PageHelper.startPage(page, size);
+        List<Abnormal> list = abnormalService.findAll();
+        List<Abnormal> resList = new ArrayList<Abnormal>();
+        for (Abnormal item : list) {
+            if (item.getValid() == 1) {
+                resList.add(item);
+            }
+        }
+        list.clear();
+        PageInfo pageInfo = new PageInfo(resList);
         return ResultGenerator.genSuccessResult(pageInfo);
     }
 }
