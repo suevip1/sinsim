37d4334f0627eba3494f6e29172f19e41cacf2a9
Hu Tong <htbasketball@163.com>
2018-05-20 02:18:34 +0800

    1.同步sinsim服务器真实最新数据 2.修复计划页面无法显示待安装/安装状态、质检状态的bug（之前理解有误，以为前端没有用到）

--- src/main/java/com/eservice/api/service/impl/MachineServiceImpl.java
+++ src/main/java/com/eservice/api/service/impl/MachineServiceImpl.java
@@ -73,78 +73,69 @@ public class MachineServiceImpl extends AbstractService<Machine> implements Mach
         } else {
             machinePlanList = machineMapper.selectPlanningMachines(orderNum, machine_strid, nameplate, location, status, machineType,dateType, query_start_time, query_finish_time);
         }
-//        for (MachinePlan itemPlan: machinePlanList) {
-//
-//                //获取机器对应task record,
-//                Condition tempCondition = new Condition(TaskRecord.class);
-//                tempCondition.createCriteria().andCondition("process_record_id = ", itemPlan.getProcessRecordID());
-//                tempCondition.createCriteria().andGreaterThanOrEqualTo("status", Constant.TASK_INITIAL);
-//                List<TaskRecord> taskRecordList = taskRecordService.findByCondition(tempCondition);
-//                HashMap<Byte, Integer> taskStatusMap = new HashMap<>();
-//                for (TaskRecord record : taskRecordList) {
-//                    if(record.getStatus().equals(Constant.TASK_PLANED)) {
-//                        if(taskStatusMap.get(Constant.TASK_PLANED) != null){
-//                            taskStatusMap.put(Constant.TASK_PLANED, taskStatusMap.get(Constant.TASK_PLANED) + 1);
-//                        }else{
-//                            taskStatusMap.put(Constant.TASK_PLANED, 1);
-//                        }
-//                    } else if(record.getStatus().equals(Constant.TASK_INSTALL_WAITING)){
-//                        if(taskStatusMap.get(Constant.TASK_INSTALL_WAITING) !=null){
-//                            taskStatusMap.put(Constant.TASK_INSTALL_WAITING,taskStatusMap.get(Constant.TASK_INSTALL_WAITING) + 1);
-//                        } else {
-//                            taskStatusMap.put(Constant.TASK_INSTALL_WAITING,1);
-//                        }
-//                    } else if(record.getStatus().equals(Constant.TASK_INSTALLING)) {
-//                        if(taskStatusMap.get(Constant.TASK_INSTALLING) != null){
-//                            taskStatusMap.put(Constant.TASK_INSTALLING, taskStatusMap.get(Constant.TASK_INSTALLING) + 1);
-//                        }else{
-//                            taskStatusMap.put(Constant.TASK_INSTALLING, 1);
-//                        }
-//                    }else if(record.getStatus().equals(Constant.TASK_INSTALLED)){
-//                        if(taskStatusMap.get(Constant.TASK_INSTALLED) != null){
-//                            taskStatusMap.put(Constant.TASK_INSTALLED, taskStatusMap.get(Constant.TASK_INSTALLED) + 1);
-//                        } else{
-//                            taskStatusMap.put(Constant.TASK_INSTALLED, 1);
-//                        }
-//                    }else if(record.getStatus().equals(Constant.TASK_QUALITY_DOING)){
-//                        if(taskStatusMap.get(Constant.TASK_QUALITY_DOING) != null){
-//                            taskStatusMap.put(Constant.TASK_QUALITY_DOING, taskStatusMap.get(Constant.TASK_QUALITY_DOING) + 1);
-//                        }else{
-//                            taskStatusMap.put(Constant.TASK_QUALITY_DOING, 1);
-//                        }
-//                    }else if(record.getStatus().equals(Constant.TASK_QUALITY_DONE)){
-//                        if(taskStatusMap.get(Constant.TASK_QUALITY_DONE) != null){
-//                            taskStatusMap.put(Constant.TASK_QUALITY_DONE, taskStatusMap.get(Constant.TASK_QUALITY_DONE) + 1);
-//                        }else{
-//                            taskStatusMap.put(Constant.TASK_QUALITY_DONE, 1);
-//                        }
-//                    }else if(record.getStatus().equals(Constant.TASK_INSTALL_ABNORMAL)){
-//                        if(taskStatusMap.get(Constant.TASK_INSTALL_ABNORMAL) != null){
-//                            taskStatusMap.put(Constant.TASK_INSTALL_ABNORMAL, taskStatusMap.get(Constant.TASK_INSTALL_ABNORMAL) + 1);
-//                        }else{
-//                            taskStatusMap.put(Constant.TASK_INSTALL_ABNORMAL, 1);
-//                        }
-//                    }else if(record.getStatus().equals(Constant.TASK_QUALITY_ABNORMAL)){
-//                        if(taskStatusMap.get(Constant.TASK_QUALITY_ABNORMAL) != null){
-//                            taskStatusMap.put(Constant.TASK_QUALITY_ABNORMAL, taskStatusMap.get(Constant.TASK_QUALITY_ABNORMAL) + 1);
-//                        }else{
-//                            taskStatusMap.put(Constant.TASK_QUALITY_ABNORMAL, 1);
-//                        }
-//                    }
-//                }
-//                itemPlan.setPlanedTaskNum(taskStatusMap.get(Constant.TASK_PLANED));
-//                itemPlan.setInstallWaitingTaskNum(taskStatusMap.get(Constant.TASK_INSTALL_WAITING));
-//                itemPlan.setInstalledTaskNum(taskStatusMap.get(Constant.TASK_INSTALLED));
-//                itemPlan.setInstallingTaskNum(taskStatusMap.get(Constant.TASK_INSTALLING));
-//                itemPlan.setInstallAbnormalTaskNum(taskStatusMap.get(Constant.TASK_INSTALL_ABNORMAL));
-//                itemPlan.setQualityDoingTaskNum(taskStatusMap.get(Constant.TASK_QUALITY_DOING));
-//                itemPlan.setQualityDoneTaskNum(taskStatusMap.get(Constant.TASK_QUALITY_DONE));
-//                itemPlan.setQualityAbnormalTaskNum(taskStatusMap.get(Constant.TASK_QUALITY_ABNORMAL));
-//
-//        }
+        for (MachinePlan itemPlan: machinePlanList) {
+
+            //获取机器对应task record,
+            Condition tempCondition = new Condition(TaskRecord.class);
+            tempCondition.createCriteria().andCondition("process_record_id = ", itemPlan.getProcessRecordID());
+            tempCondition.createCriteria().andGreaterThanOrEqualTo("status", Constant.TASK_INITIAL);
+            List<TaskRecord> taskRecordList = taskRecordService.findByCondition(tempCondition);
+            HashMap<Byte, Integer> taskStatusMap = new HashMap<>();
+            for (TaskRecord record : taskRecordList) {
+                if(record.getStatus().equals(Constant.TASK_INSTALL_WAITING)){
+                    if(taskStatusMap.get(Constant.TASK_INSTALL_WAITING) !=null){
+                        taskStatusMap.put(Constant.TASK_INSTALL_WAITING,taskStatusMap.get(Constant.TASK_INSTALL_WAITING) + 1);
+                    } else {
+                        taskStatusMap.put(Constant.TASK_INSTALL_WAITING,1);
+                    }
+                } else if(record.getStatus().equals(Constant.TASK_INSTALLING)) {
+                    if(taskStatusMap.get(Constant.TASK_INSTALLING) != null){
+                        taskStatusMap.put(Constant.TASK_INSTALLING, taskStatusMap.get(Constant.TASK_INSTALLING) + 1);
+                    }else{
+                        taskStatusMap.put(Constant.TASK_INSTALLING, 1);
+                    }
+                }else if(record.getStatus().equals(Constant.TASK_INSTALLED)){
+                    if(taskStatusMap.get(Constant.TASK_INSTALLED) != null){
+                        taskStatusMap.put(Constant.TASK_INSTALLED, taskStatusMap.get(Constant.TASK_INSTALLED) + 1);
+                    } else{
+                        taskStatusMap.put(Constant.TASK_INSTALLED, 1);
+                    }
+                }else if(record.getStatus().equals(Constant.TASK_QUALITY_DOING)){
+                    if(taskStatusMap.get(Constant.TASK_QUALITY_DOING) != null){
+                        taskStatusMap.put(Constant.TASK_QUALITY_DOING, taskStatusMap.get(Constant.TASK_QUALITY_DOING) + 1);
+                    }else{
+                        taskStatusMap.put(Constant.TASK_QUALITY_DOING, 1);
+                    }
+                }else if(record.getStatus().equals(Constant.TASK_QUALITY_DONE)){
+                    if(taskStatusMap.get(Constant.TASK_QUALITY_DONE) != null){
+                        taskStatusMap.put(Constant.TASK_QUALITY_DONE, taskStatusMap.get(Constant.TASK_QUALITY_DONE) + 1);
+                    }else{
+                        taskStatusMap.put(Constant.TASK_QUALITY_DONE, 1);
+                    }
+                }else if(record.getStatus().equals(Constant.TASK_INSTALL_ABNORMAL)){
+                    if(taskStatusMap.get(Constant.TASK_INSTALL_ABNORMAL) != null){
+                        taskStatusMap.put(Constant.TASK_INSTALL_ABNORMAL, taskStatusMap.get(Constant.TASK_INSTALL_ABNORMAL) + 1);
+                    }else{
+                        taskStatusMap.put(Constant.TASK_INSTALL_ABNORMAL, 1);
+                    }
+                }else if(record.getStatus().equals(Constant.TASK_QUALITY_ABNORMAL)){
+                    if(taskStatusMap.get(Constant.TASK_QUALITY_ABNORMAL) != null){
+                        taskStatusMap.put(Constant.TASK_QUALITY_ABNORMAL, taskStatusMap.get(Constant.TASK_QUALITY_ABNORMAL) + 1);
+                    }else{
+                        taskStatusMap.put(Constant.TASK_QUALITY_ABNORMAL, 1);
+                    }
+                }
+            }
+            itemPlan.setInstallWaitingTaskNum(taskStatusMap.get(Constant.TASK_INSTALL_WAITING));
+            itemPlan.setInstalledTaskNum(taskStatusMap.get(Constant.TASK_INSTALLED));
+            itemPlan.setInstallingTaskNum(taskStatusMap.get(Constant.TASK_INSTALLING));
+            itemPlan.setInstallAbnormalTaskNum(taskStatusMap.get(Constant.TASK_INSTALL_ABNORMAL));
+            itemPlan.setQualityDoingTaskNum(taskStatusMap.get(Constant.TASK_QUALITY_DOING));
+            itemPlan.setQualityDoneTaskNum(taskStatusMap.get(Constant.TASK_QUALITY_DONE));
+            itemPlan.setQualityAbnormalTaskNum(taskStatusMap.get(Constant.TASK_QUALITY_ABNORMAL));
+        }
 
         return machinePlanList;
-
     }
 
     //selectConfigMachine
